98b986069ff186ef17e1d97933dc228b8a78bd96
wpy <1251499395@qq.com>
2018-06-28 20:08:13 +0800

    all file

---
+++ app/src/main/java/test/haixi/com/androidbasicgame/StartActivity.java
@@ -0,0 +1,13 @@
+package test.haixi.com.androidbasicgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
